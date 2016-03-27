dba113aa0dd7499cf1fbe9f236ad139818d4f8d2
Daniel Luz <daniel_almeida_luz@hotmail.com>
2016-03-27 16:09:30 -0300

    added ground creation from string

--- Game/Level.elm
+++ Game/Level.elm
@@ -1,1 +1,10 @@
 module Game.Level where
+import Game.Model as Game exposing (Grounds)
+
+loadLevel : Integer -> List Game.Grounds
+loadLevel n =
+  let
+    groundRect1 = Collision2D.rectangle 0 0 32 32
+    groundRect2 = Collision2D.rectangle 32 0 32 32
+  in
+    [Ground 32 0 groundRect2, Ground 0 0 groundRect1]
--- Game/Model.elm
+++ Game/Model.elm
@@ -2,6 +2,8 @@ module Game.Model where
 
 import Collision2D
 import List exposing (..)
+import String exposing (..)
+-- import Game.Level as Level exposing (..)
 
 (gameWidth,gameHeight) = (600,400)
 (halfWidth,halfHeight) = (300,200)
@@ -39,10 +41,99 @@ defaultGame : Game
 defaultGame =
   let
     playerRect = Collision2D.rectangle 0 250 32 32
-    groundRect1 = Collision2D.rectangle 0 0 32 32
-    groundRect2 = Collision2D.rectangle 32 0 32 32
+    loadedGrounds = loadLevel(1)
   in
     { state = Play 
     , player = Player 0 250 0 0 Left playerRect False
-    , grounds = [Ground 32 0 groundRect2, Ground 0 0 groundRect1]
+    , grounds = loadedGrounds
     }
+
+loadLevel : Int -> List Ground
+loadLevel n =
+  let
+    level' = level
+    -- groundRect1 = Collision2D.rectangle 0 0 32 32
+    -- groundRect2 = Collision2D.rectangle 32 0 32 32
+  in
+    asciiToGround level'
+    -- [Ground 32 0 groundRect2, Ground 0 0 groundRect1]
+
+
+-- level = """
+--   * *****         *
+--   *               *
+-- """
+level = """
+  *****************
+  *                         *
+  *                         *
+  *                     *
+  *                         *
+  *     ******              *
+  *                         *
+  *                         *
+  *                         *
+  *                         *
+  *              *****      *
+  *                         *          
+  *                         *
+  *                         *
+  *      ****               *
+  *                         *
+  ***       *****************
+"""
+
+asciiToGround : String -> List Ground
+asciiToGround string =
+  let 
+    splitted = String.split "\n" string
+    lines = splitted
+      |> withIndex
+  in
+    List.map lineToGround lines
+      |> List.concat
+  --   positions = lines
+  --     |> withIndex
+  --     |> concatMap parseLine
+  --   count = length positions
+  --   makeGround (row,col) = Model.makeGround row col
+  -- in map makeGround positions
+lineToGround : (Int,String) -> List Ground
+lineToGround line =
+  let
+    index = fst line
+    line' = snd line
+    chars' = line'
+      |> toList
+      |> List.map fromChar
+      |> withIndex
+  in
+    makeGroundLine index chars'
+
+makeGroundLine : Int -> List (Int,String) -> List Ground
+makeGroundLine index line =
+  let
+    yPos = yFunc index
+    xPos =
+      parseLine line
+  in
+    List.map (\x -> Ground x yPos (Collision2D.rectangle x yPos 32 32)) xPos
+
+parseLine : List (Int,String) -> List Float
+parseLine line =
+  List.filter (\l -> snd l == "*") line
+    |> List.map (\l -> xFunc (fst l))
+
+yFunc index =
+  -400 + 32 * index
+  |> Basics.toFloat
+
+xFunc index =
+  -350 + 32 * index
+  |> Basics.toFloat
+
+withIndex list = 
+  list |> zip [0..List.length list]
+
+zip xs ys = 
+  List.map2 (,) xs ys
