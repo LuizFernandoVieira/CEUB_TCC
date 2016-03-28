e8ec27a7253fa0bfc7c227098b3a58389583c848
Daniel Luz <daniel_almeida_luz@hotmail.com>
2016-03-28 20:42:28 -0300

    Refactored ground creation

--- Game/Level.elm
+++ Game/Level.elm
@@ -1,10 +1,96 @@
 module Game.Level where
-import Game.Model as Game exposing (Grounds)
 
-loadLevel : Integer -> List Game.Grounds
+import String exposing (split,toList,fromChar)
+import List exposing (..)
+
+loadLevel : Int -> List (Float,Float)
 loadLevel n =
   let
-    groundRect1 = Collision2D.rectangle 0 0 32 32
-    groundRect2 = Collision2D.rectangle 32 0 32 32
+    level' = level
   in
-    [Ground 32 0 groundRect2, Ground 0 0 groundRect1]
+    asciiToCoordinates level'
+
+level = """
+  *****************
+  *                         *
+  *                         *
+  *                     *
+  *                         *
+  *     ******              *
+  *                         *
+  *                         *
+  *                         *
+  *                         *
+  *              *****      *
+  *                         *          
+  *                         *
+  *                         *
+  *      ****               *
+  *                         *
+  ***       *****************
+"""
+
+asciiToCoordinates : String -> List (Float,Float)
+asciiToCoordinates string =
+  let 
+    splitted = String.split "\n" string
+      |> List.reverse
+    indexedlines = splitted
+      |> withIndex
+  in
+    map parseLineToTuples indexedlines
+      |> concat
+      |> calculateCoordinates
+
+parseLineToTuples : (Int,String) -> List (Int,Int)
+parseLineToTuples line =
+  let
+    yPos = fst line
+    xPosList =
+      snd line
+      |> parseXCoordinates
+  in
+    zipPositions yPos xPosList
+
+calculateCoordinates : List (Int,Int) -> List (Float,Float)
+calculateCoordinates positions =
+  map calculateTuple positions
+  -- map (\tuple -> (xFunc (fst tuple), yFunc (snd tuple))) positions
+
+calculateTuple : (Int,Int) -> (Float,Float)
+calculateTuple (x,y) =
+  (xFunc x, yFunc y)
+
+parseXCoordinates : String -> List Int
+parseXCoordinates line =
+  line 
+    |> toStringList 
+    |> withIndex
+    |> parseLine
+
+parseLine : List (Int,String) -> List Int
+parseLine line =
+  filter (\l -> snd l == "*") line
+    |> map fst
+
+zipPositions : Int -> List Int -> List (Int,Int)
+zipPositions yPos xPosList =
+  map (\x -> (x,yPos)) xPosList
+
+yFunc index =
+  -400 + 32 * index
+  |> toFloat
+
+xFunc index =
+  -350 + 32 * index
+  |> toFloat
+
+withIndex list = 
+  list |> zip [0..length list]
+
+toStringList : String -> List String
+toStringList str =
+  map fromChar <| toList str
+
+zip xs ys = 
+  map2 (,) xs ys
--- Game/Model.elm
+++ Game/Model.elm
@@ -2,8 +2,7 @@ module Game.Model where
 
 import Collision2D
 import List exposing (..)
-import String exposing (..)
--- import Game.Level as Level exposing (..)
+import Game.Level exposing (..)
 
 (gameWidth,gameHeight) = (600,400)
 (halfWidth,halfHeight) = (300,200)
@@ -42,94 +41,14 @@ defaultGame =
   let
     playerRect = Collision2D.rectangle 0 250 32 32
     loadedGrounds = loadLevel(1)
+    grounds' = loadLevel(1)
+                |> buildGround
   in
     { state = Play 
     , player = Player 0 250 0 0 Left playerRect False
-    , grounds = loadedGrounds
+    , grounds = grounds'
     }
 
-loadLevel : Int -> List Ground
-loadLevel n =
-  let
-    level' = level
-    -- groundRect1 = Collision2D.rectangle 0 0 32 32
-    -- groundRect2 = Collision2D.rectangle 32 0 32 32
-  in
-    asciiToGround level'
-    -- [Ground 32 0 groundRect2, Ground 0 0 groundRect1]
-
-
--- level = """
---   * *****         *
---   *               *
--- """
-level = """
-  *****************
-  *                         *
-  *                         *
-  *                     *
-  *                         *
-  *     ******              *
-  *                         *
-  *                         *
-  *                         *
-  *                         *
-  *              *****      *
-  *                         *          
-  *                         *
-  *                         *
-  *      ****               *
-  *                         *
-  ***       *****************
-"""
-
-asciiToGround : String -> List Ground
-asciiToGround string =
-  let 
-    splitted = String.split "\n" string
-      |> List.reverse
-    lines = splitted
-      |> withIndex
-  in
-    List.map lineToGround lines
-      |> List.concat
-
-lineToGround : (Int,String) -> List Ground
-lineToGround line =
-  let
-    index = fst line
-    line' = snd line
-    chars' = line'
-      |> toList
-      |> List.map fromChar
-      |> withIndex
-  in
-    makeGroundLine index chars'
-
-makeGroundLine : Int -> List (Int,String) -> List Ground
-makeGroundLine index line =
-  let
-    yPos = yFunc index
-    xPos =
-      parseLine line
-  in
-    List.map (\x -> Ground x yPos (Collision2D.rectangle x yPos 32 32)) xPos
-
-parseLine : List (Int,String) -> List Float
-parseLine line =
-  List.filter (\l -> snd l == "*") line
-    |> List.map (\l -> xFunc (fst l))
-
-yFunc index =
-  -400 + 32 * index
-  |> Basics.toFloat
-
-xFunc index =
-  -350 + 32 * index
-  |> Basics.toFloat
-
-withIndex list = 
-  list |> zip [0..List.length list]
-
-zip xs ys = 
-  List.map2 (,) xs ys
+buildGround : List (Float,Float) -> List Ground
+buildGround coordinates =
+  List.map (\(x,y) -> Ground x y (Collision2D.rectangle x y 32 32)) coordinates
